103c1c0aedc10aeebe907c4dff7b37a143e59a2d
NjabuloJ <fanajb65@gmail.com>
2025-08-01 20:09:29 +0200

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,66 @@
+const { cmd } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "owner",
+    react: "✅", 
+    desc: "Get owner number",
+    category: "main",
+    filename: __filename
+}, 
+async (conn, mek, m, { from }) => {
+    try {
+        const ownerNumber = config.OWNER_NUMBER; // Fetch owner number from config
+        const ownerName = config.OWNER_NAME;     // Fetch owner name from config
+
+        const vcard = 'BEGIN:VCARD\n' +
+                      'VERSION:3.0\n' +
+                      `FN:${ownerName}\n` +  
+                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
+                      'END:VCARD';
+
+        // Send the vCard
+        const sentVCard = await conn.sendMessage(from, {
+            contacts: {
+                displayName: ownerName,
+                contacts: [{ vcard }]
+            }
+        });
+
+        // Send the owner contact message with image and audio
+        await conn.sendMessage(from, {
+            image: { url: 'https://files.catbox.moe/iksjgm.jpg' }, // Image URL from your request
+            caption: `
+- . *Here is the owner details*
+- . *Name* - ${ownerName}
+- . *Number* ${ownerNumber}
+- . *Version*: 2.0.0 Beta`,
+            contextInfo: {
+                mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399999197102@newsletter',
+                    newsletterName: '╭••➤®Njabulo Jb',
+                    serverMessageId: 143
+                    }
+            }
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+
+    } catch (error) {
+        console.error(error);
+        reply(`An error occurred: ${error.message}`);
+    }
+});
