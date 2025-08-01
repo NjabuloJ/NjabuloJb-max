28cfdec7f4e1b9e8a46e2dd998a40eab64e145ba
NjabuloJ <fanajb65@gmail.com>
2025-08-01 19:11:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
+BOT_NAME: process.env.BOT_NAME || "NjabuloJb-max",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
 // type sticker pack name 
