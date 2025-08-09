48842baf4380d5db73020a1845a4297f40395bf8
NjabuloJ <fanajb65@gmail.com>
2025-08-09 13:49:25 +0200

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -6,7 +6,7 @@ cmd({
     alias: ["speed","pong"],use: '.ping',
     desc: "Check bot's response time.",
     category: "main",
-    react: "⚡",
+    react: "🏓",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, sender, reply, }) => {
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *CRISS-AI SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *Njabulo Jb: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -46,7 +46,19 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
                     serverMessageId: 143
                 }
             }
-        }, { quoted: mek });
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
 
     } catch (e) {
         console.error("Error in ping command:", e);
