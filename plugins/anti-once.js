84fe19c4841c795223e85651facc998d8ff0abd3
NjabuloJ <fanajb65@gmail.com>
2025-08-09 14:18:41 +0200

    Update anti-once.js

--- plugins/anti-once.js
+++ plugins/anti-once.js
@@ -14,10 +14,31 @@ cmd({
 
     if (!match.quoted) {
       return await client.sendMessage(from, {
-        text: "*🍁 Please reply to a view once message!*"
-      }, { quoted: message });
-    }
-
+        text: "*🍁 Please reply to a view once message!*",
+        contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399999197102@newsletter',
+                    newsletterName: '╭••➤®Njabulo Jb',
+                    serverMessageId: 143
+                }
+            }
+}, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+}
     const buffer = await match.quoted.download();
     const mtype = match.quoted.mtype;
     const options = { quoted: message };
