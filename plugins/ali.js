84f6ca6de67d67022b9585715d189e2af9e9bd64
NjabuloJ <fanajb65@gmail.com>
2025-08-09 14:04:25 +0200

    Create ali.js

---
+++ plugins/ali.js
@@ -0,0 +1,60 @@
+const { cmd } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const config = require('../config');
+
+cmd({
+    pattern: "alives",
+    alias: ["status", "online", "a"],
+    desc: "Check bot is alive or not",
+    category: "main",
+    react: "🏓",
+    filename: __filename
+},
+async (conn, mek, m, { from, sender, reply }) => {
+    try {
+        const status = `
+*Uptime:* ${runtime(process.uptime())}`;
+
+        const buttons = [
+            {
+                buttonId: 'menu-btn',
+                buttonText: { displayText: 'Menu' },
+                type: 1
+            }
+        ];
+
+        await conn.sendMessage(from, {
+            image: { url: config.MENU_IMAGE_URL },
+            caption: status,
+            buttons,
+            headerType: 4,
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 1000,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363399999197102@newsletter',
+                    newsletterName: '╭••➤®Njabulo Jb',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+
+    } catch (e) {
+        console.error("Alive Error:", e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
