ae36fd69dccb311e06bcc4df2f911a469d484d1d
NjabuloJ <fanajb65@gmail.com>
2025-08-09 20:15:09 +0200

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -46,7 +46,19 @@ cmd({
                     serverMessageId: 143
                 }
             }
-        }, { quoted: mek });
+          }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
             // Add delay between sends to avoid rate limiting
             await new Promise(resolve => setTimeout(resolve, 1000));
         }
