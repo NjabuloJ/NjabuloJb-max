56ce03ec39d37d037695d12644c8d17baf20942c
NjabuloJ <fanajb65@gmail.com>
2025-08-10 22:56:15 +0200

    Update ytdl-fast.js

--- plugins/ytdl-fast.js
+++ plugins/ytdl-fast.js
@@ -14,7 +14,32 @@ cmd({
     filename: __filename 
 }, async (conn, mek, m, { from, sender, reply, q }) => { 
     try {
-        if (!q) return reply("Please provide a song name or YouTube link.");
+        if (!q) {
+        await conn.sendMessage(m.chat, {
+      text: "Please provide a song name or YouTube link..",
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
 
         const yt = await ytsearch(q);
         if (!yt.results.length) return reply("No results found!");
