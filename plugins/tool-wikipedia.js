56aa14d2e707059062cc17235cce2c3cfc2bcb27
NjabuloJ <fanajb65@gmail.com>
2025-08-10 22:25:02 +0200

    Update tool-wikipedia.js

--- plugins/tool-wikipedia.js
+++ plugins/tool-wikipedia.js
@@ -33,8 +33,30 @@ cmd({
     if (thumb) {
       await conn.sendMessage(m.chat, {
         image: { url: thumb },
-        caption: message
-      });
+        caption: message,
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
     } else {
       await reply(message);
     }
