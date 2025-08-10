ab49da1435a8b90c9725132d1b5002502b453f26
NjabuloJ <fanajb65@gmail.com>
2025-08-10 23:16:56 +0200

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -40,7 +40,31 @@ cmd({
         } });
     }
 
-        await reply(`🔍 Searching images for "${query}"...`);
+                await conn.sendMessage(m.chat, {
+      text: `🔍 Searching images for "${query}"...`,
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
 
         const url = `https://apis.davidcyriltech.my.id/googleimage?query=${encodeURIComponent(query)}`;
         const response = await axios.get(url);
