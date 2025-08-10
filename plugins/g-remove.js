180c9aa7e5d103a984055d440feae8d10424750b
NjabuloJ <fanajb65@gmail.com>
2025-08-11 01:52:12 +0200

    Update g-remove.js

--- plugins/g-remove.js
+++ plugins/g-remove.js
@@ -14,7 +14,7 @@ async (conn, mek, m, {
     // Check if the command is used in a group
     if (!isGroup)
     await conn.sendMessage(m.chat, {
-      text: "* This command can only be used in groups.*",
+      text: "*This command can only be used in groups.*",
       contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
@@ -72,7 +72,7 @@ async (conn, mek, m, {
     // Check if the bot is an admin
     if (!isBotAdmins) 
   await conn.sendMessage(m.chat, {
-      text: "* I need to be an admin to use this command.*",
+      text: "*I need to be an admin to use this command.*",
       contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
@@ -103,7 +103,31 @@ async (conn, mek, m, {
     } else if (q && q.includes("@")) {
         number = q.replace(/[@\s]/g, ''); // If mentioning a user
     } else {
-        return reply("❌ Please reply to a message or mention a user to remove.");
+    await conn.sendMessage(m.chat, {
+      text: "* Please reply to a message or mention a user to remove*",
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
     }
 
     const jid = number + "@s.whatsapp.net";
