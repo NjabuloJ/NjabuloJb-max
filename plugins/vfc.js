3333d5e6f02709934a18f21886deb141a47af82f
NjabuloJ <fanajb65@gmail.com>
2025-08-14 21:18:17 +0200

    @njabulo

---
+++ plugins/vfc.js
@@ -0,0 +1,141 @@
+const fs = require('fs');
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+
+
+
+//vcf//
+
+cmd({
+    pattern: 'savecontact',
+    alias: ["vcf","scontact","savecontacts"],
+    desc: 'gc vcard',
+    category: 'tools',
+    filename: __filename
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) 
+       await conn.sendMessage(from, {
+      text: "This command is for groups only."
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+  } catch (error) {
+    console.error("❌ Error in repeat command:", error);
+    reply("❎ An error occurred while processing your request.");
+  }
+});
+
+        if (!isOwner) 
+await conn.sendMessage(from, {
+      text: "*_This command is for the owner only_*",
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+  } catch (error) {
+    console.error("❌ Error in repeat command:", error);
+    reply("❎ An error occurred while processing your request.");
+  }
+});
+
+        let card = quoted || m; // Handle if quoted message exists
+        let cmiggc = groupMetadata;
+        const { participants } = groupMetadata;
+        
+        let orgiggc = participants.map(a => a.id);
+        let vcard = '';
+        let noPort = 0;
+        
+        for (let a of cmiggc.participants) {
+            vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:[${noPort++}] +${a.id.split("@")[0]}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`;
+        }
+
+        let nmfilect = './contacts.vcf';
+        reply('Saving ' + cmiggc.participants.length + ' participants contact');
+
+        fs.writeFileSync(nmfilect, vcard.trim());
+        await sleep(2000);
+
+        await conn.sendMessage(from, {
+            document: fs.readFileSync(nmfilect), 
+            mimetype: 'text/vcard', 
+            fileName: 'status-save.vcf', 
+            caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*`;
+      contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363399999197102@newsletter',
+                        newsletterName: '╭••➤®Njabulo Jb',
+                        serverMessageId: 143
+                    }
+               }
+             }, { quoted: {
+            key: {
+                fromMe: false,
+                participant: `0@s.whatsapp.net`,
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: "✆︎NנɐႦυℓσ נႦ verified",
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`
+                }
+            }
+        } });
+  } catch (error) {
+    console.error("❌ Error in repeat command:", error);
+    reply("❎ An error occurred while processing your request.");
+  }
+});
+
+
+        fs.unlinkSync(nmfilect); // Cleanup the file after sending
+    } catch (err) {
+        reply(err.toString());
+    }
+});
+
+
