f1e1dc76ee52c360d1dca79c51f5a25c351bd079
NjabuloJ <fanajb65@gmail.com>
2025-08-19 13:21:29 +0200

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -56,12 +56,12 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
         }, { quoted: {
             key: {
                 fromMe: false,
-                participant: `0@s.whatsapp.net`,
+                participant: `0@s.MetaAi.net`,
                 remoteJid: "status@broadcast"
             },
             message: {
                 contactMessage: {
-                    
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Bot\nEND:VCARD`    
             }
             }
         } });
